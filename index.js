7a26c78a2030636fee6da0cd636c4952de5d6620
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-15 08:37:19 -0300

    Alteração

---
+++ index.js
@@ -0,0 +1,16 @@
+const sum = (num1, num2) => {
+    return num1 + num2;
+}
+const minus = (num1, num2) => {
+    return num1 - num2;
+
+}
+const mult = (num1, num2) => {
+    return num1 * num2;
+    
+}
+const divide = (num1, num2) => {
+    return num1 / num2;
+    
+}
+module.exports = {sum, minus, mult, divide}
